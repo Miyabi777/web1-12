c0e0be5ee52e6df54922f0b10badb142c5d287cb
Miyabi777 <kmgi77@icloud.com>
2017-06-26 17:59:43 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -43,3 +43,14 @@ document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').word.value);
   return false;
 };
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0)
+    console.log("Fizz,Buzz");
+  else if (i % 3 == 0)
+    console.log("Fizz");
+  else if (i % 5 == 0)
+    console.log("Buzz");
+  else  // iが3の倍数でも5の倍数でもない
+    console.log(i);
+}
